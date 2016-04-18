3c509acb278d9de0cc5066fd8dd1ac5937078f52
e.gonzalez@utexas.edu <e.gonzalez@utexas.edu>
2016-04-17 19:52:38 -0500

    CarsonSucksBranch

---
+++ Team_Five_Final_Project/Models/CarsonSucks.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team_Five_Final_Project.Models
+{
+    public class CarsonSucks
+    {
+    }
+}
