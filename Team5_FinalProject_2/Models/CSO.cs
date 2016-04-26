9c1dfaeb72dbc846d8bbb0a39453c75d30bc4a79
cjy276 <cjyouman@utexas.edu>
2016-04-26 15:58:57 -0500

    addCSO

---
+++ Team5_FinalProject_2/Models/CSO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Team5_FinalProject_2.Models
+{
+    public class CSO
+    {
+
+        public Int32 CSOID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Please enter a valid Email Address")]
+        public String Email { get; set; }
+
+    }
+}
